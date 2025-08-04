c1cb8647de0df3362f116ec5f01dff9a0b6dd637
Axel Munoz <naclif30@gmail.com>
2025-08-03 23:36:14 -0400

    feat:jwt added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,43 @@
+const { response } = require("express");
+const bcrypt = require('bcryptjs')
 const Usuario = require("../models/Usuario");
+const { generarJWT } = require("../helpers/jwt");
 
 
-const crearUsuario = async(req,res) => {
+const crearUsuario = async(req,res = response) => {
     
-    // const { name , email ,password } = req.body;
+    const { email ,password } = req.body;
 
-    const usuario = new Usuario(req.body)
-    await usuario.save();
+    try {
+
+        let usuario = await Usuario.findOne({ email })
+        if(usuario) {
+            return res.status(400).json({
+                ok:false,
+                msg: 'Un usuario existe con este correo'
+            })
+        }
+        usuario = new Usuario(req.body)
+
+        //Encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt)
+
+        await usuario.save();
+        //Generar JWT 
+        const token = await generarJWT(usuario.id , usuario.name)
+        res.status(201).json({
+            ok:true,
+            uid: usuario.id,
+            name : usuario.name,
+            token
+        })        
+    } catch (error) {
+        res.status(500).json({
+            ok:false,
+            msg: 'Por favor hable con el administrador'
+        })
+    }
     
     res.json({
         ok:true,
@@ -14,24 +45,61 @@ const crearUsuario = async(req,res) => {
     })
 }
 
-const loginUsuario = (req,res) => {
+const loginUsuario = async(req,res) => {
 
     const { email ,password } = req.body;
-    
 
-    res.status(201).json({
-        ok:true,
-        msg:'login',
-        email ,
-        password
-    })
+    try {
+
+        const usuario = await Usuario.findOne({ email })
+        if(!usuario) {
+            return res.status(400).json({
+                ok:false,
+                msg: 'Un usuario no existe con ese email'
+            })
+        }
+
+        //Confirmar los passwords
+
+        const validPassword = bcrypt.compareSync(password , usuario.password)
+
+        if(!validPassword){
+            return res.status(400).json({
+                ok:false , 
+                msg: 'Password Incorrecto'
+            })
+        }
+
+        //Generar JWT
+        const token = await generarJWT(usuario.id , usuario.name)
+
+        res.json({
+            ok:true,
+            uid: usuario.id,
+            name : usuario.name,
+            token
+        })        
+        
+    } catch (error) {
+        res.status(500).json({
+            ok:false,
+            msg: 'Por favor hable con el administrador'
+        })
+        
+    }
+    
 }
 
-const revalidarToken =  (req,res) => {
-    console.log('Se solicito el /');
+const revalidarToken =  async(req,res) => {
+    
+    const {uid , name} = req
+    
+
+    const token = await generarJWT(uid , name)
+
     res.json({
         ok:true,
-        msg:'renew'
+        token
     })
 }
 
